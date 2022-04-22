acde0f0d5a7049942c45f29d6734d7fdf8cf845c
Graziano <graziano@pop-os.localdomain>
2022-04-22 20:36:36 -0300

    improved interface

--- airconsole.gd
+++ airconsole.gd
@@ -1,27 +1,52 @@
 extends Node
+class_name AirConsole
 
 signal onMessage(device_id, data)
 signal onConnect(device_id)
 signal onDisconnect(device_id)
+signal onActivePlayersChange(player_number)
 
 var _airconsole: JavaScriptObject
-var _cb_onMessage = JavaScript.create_callback(self, "_onMessage")
-var _cb_onConnect = JavaScript.create_callback(self, "_onConnect")
-var _cb_onDisconnect = JavaScript.create_callback(self, "_onDisconnect")
+var _callbacks: Array = []
+var ready: bool = false
 
+func _create_cb(f: String):
+	var cb = JavaScript.create_callback(self, f)
+	_callbacks.append(cb)
+	return cb
+	
 func _init():
 	if OS.get_name() != "HTML5":
 		print("can't load airconsole, not on a HTML environment")
 		return
 
-	_airconsole = JavaScript.create_object("AirConsole")
-	_connect_callbacks()
-	print_debug("Airconsole initialized")
+	JavaScript.eval("airconsole = new AirConsole({device_motion: 50})")
+	_airconsole = JavaScript.get_interface("airconsole")
 
+	_connect_callbacks()
+	print_debug("AirConsole initialized")
+	ready = true
+
+#functions
+func set_active_players(max_players: int):
+	_airconsole.setActivePlayers(max_players)
+
+func convert_device_id_to_player_number(device_id: int):
+	return int(_airconsole.convertDeviceIdToPlayerNumber(device_id))
+
+func convert_player_number_to_device_id(player_id: int):
+	return int(_airconsole.convertPlayerNumberToDeviceId(player_id))
+
+func message(device_id: int, data):
+	_airconsole.message(device_id, data)
+
+#callbacks
 func _connect_callbacks():
-	_airconsole.onMessage = _cb_onMessage
-	_airconsole.onConnect = _cb_onConnect
-	_airconsole.onDisconnect = _cb_onDisconnect
+	_airconsole.onMessage = _create_cb("_onMessage")
+	_airconsole.onConnect = _create_cb("_onConnect")
+	_airconsole.onDisconnect = _create_cb("_onDisconnect")
+	_airconsole.onActivePlayersChange = _create_cb("_onActivePlayersChange")
+	_airconsole.onDeviceMotion = _create_cb("_onDeviceMotion")
 
 func _onMessage(args):
 	emit_signal("onMessage", args[0], args[1])
@@ -31,3 +56,9 @@ func _onConnect(device_id):
 
 func _onDisconnect(device_id):
 	emit_signal("onDisconnect", device_id)
+
+func _onActivePlayersChange(player_number):
+	emit_signal("onActivePlayersChange", player_number)
+
+func _onDeviceMotion(data):
+	print(data)
